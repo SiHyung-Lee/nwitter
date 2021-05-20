bb2e0f64a67c385f90b5b2f7b1b32f9590dd43dc
SiHyung Lee <buzzcatfish@gmail.com>
2021-05-21 00:23:56 +0900

    Realtime Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,9 +5,13 @@ const Home = () => {
     const [nweet, setNweet] = useState('');
     const [nweets, setNweets] = useState([]);
     const getNweets = async () => {
-        const data = await dbService.collection('nweets').get();
-        data.forEach((document) => {
-            console.log(document.data());
+        const dbNweets = await dbService.collection('nweets').get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
         });
     };
     useEffect(() => {
@@ -38,6 +42,13 @@ const Home = () => {
                 />
                 <input type='submit' value='Nweet' />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
